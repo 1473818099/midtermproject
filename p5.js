71010106104a3b7ab791be0269c60c442401c8a8
KevinRen9021 <1473818099@qq.com>
2025-03-13 13:46:43 -0400

    1

---
+++ p5.js
@@ -0,0 +1,61 @@
+let mic;
+let soundP, soundL, soundK;
+
+function preload() {
+  soundFormats('wav', 'mp3');
+  
+  soundP = loadSound('Music/sound1.mp3');
+  soundL = loadSound('Music/sound2.mp3');
+  soundK = loadSound('Music/sound3.mp3');
+}
+
+function setup() {
+  createCanvas(400, 400);
+
+  mic = new p5.AudioIn();
+  mic.start();
+
+  textSize(20);
+  textAlign(CENTER, CENTER);
+  fill(255);
+}
+
+function draw() {
+  background(0);
+
+  let miclevel = mic.getLevel();
+  let circleSize = map(volume, 0, 1, 20, 400);
+  fill(255, 0, 0);
+  ellipse(width / 2, height / 2, circleSize, circleSize);
+
+  fill(255);
+  text("Press 'P', 'L', or 'K' to play sounds", width / 2, height - 40);
+
+  let playbackRate = map(mouseX, 0, width, 0.5, 2.0);
+  soundP.rate(playbackRate);
+  soundL.rate(playbackRate);
+  soundK.rate(playbackRate);
+  
+  text('Playback Rate: ' + playbackRate.toFixed(2), width / 2, 30);
+}
+
+function keyPressed() {
+  console.log("Key pressed:", key);
+  
+  if (key.toLowerCase() === 'p') {
+    playSound(soundP);
+  } else if (key.toLowerCase() === 'l') {
+    playSound(soundL);
+  } else if (key.toLowerCase() === 'k') {
+    playSound(soundK);
+  }
+}
+
+function playSound(sound) {
+  if (sound.isLoaded()) {
+    sound.play();
+    console.log("Playing sound:", sound);
+  } else {
+    console.log("Sound not loaded yet.");
+  }
+}
